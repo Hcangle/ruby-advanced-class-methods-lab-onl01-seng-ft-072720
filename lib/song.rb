a40f7c17cd4154d4b7d76db5302ba1c69ae96df3
Hannah Angle <hannahcangle@gmail.com>
2020-08-08 03:55:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,62 +1,35 @@
-class Song 
+class Song
   attr_accessor :name, :artist_name
   @@all = []
-  self.class.all << self 
-end
-def self.create	
-    song = Song.new	
-    song.save	
-    song	
-  end	
 
-  def self.new_by_name(song_name)	
-    song = self.new	
-    song.name = song_name	
-    song	
-  end	
+  def self.all
+    @@all
+  end
 
-  def self.create_by_name(song_name)	
-    song = self.create	
-    song.name = song_name	
-    song	
-  end	
+  def save
+    self.class.all << self
+  end
 
-  def self.find_by_name(song_name)	
-    self.all.detect{|s| s.name == song_name}	
-  end	
-
-  def self.find_or_create_by_name(song_name)	
-    self.find_by_name(song_name) || self.create_by_name(song_name)	
-  end	
-
-  def self.alphabetical	
-    self.all.sort_by{|s| s.name}	
-  end	
-
-  def self.new_from_filename(filename)	
-    parts = filename.split(" - ")	
-    artist_name = parts[0]	
-    song_name = parts[1].gsub(".mp3", "")	
-
-    song = self.new	
-    song.name = song_name	
-    song.artist_name = artist_name	
-    song	
-  end	
-
-  def self.create_from_filename(filename)	
-    parts = filename.split(" - ")	
-    artist_name = parts[0]	
-    song_name = parts[1].gsub(".mp3", "")	
-
-    song = self.create	
-    song.name = song_name	
-    song.artist_name = artist_name	
-    song	
-  end	
-
-  def self.destroy_all	
-    self.all.clear	
-  end	
-
+  def self.create
+    song = self.new
+    #@@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
 end
